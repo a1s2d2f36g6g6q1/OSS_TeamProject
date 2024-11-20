bb788c0957f6e8f232c561a6fd083addf7a6e24c
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-20 14:19:36 +0900

    뼈대 작성

--- Tetris/Tetris.c
+++ Tetris/Tetris.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include "Tetris.h"
 
+#define ROWS 20
+#define COLS 10
+
+static char board[ROWS][COLS];
+
+void initializeBoard() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            board[i][j] = '.';  // 빈 공간을 '.'으로 표시
+        }
+    }
+}
+
+void printBoard() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void startTetris() {
     printf("Starting Tetris game...\n");
+
+    initializeBoard();
+
+    printBoard();
 }
