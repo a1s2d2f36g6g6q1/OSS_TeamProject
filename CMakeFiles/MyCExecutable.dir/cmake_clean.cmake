0a55603e5aa92b5489308e8cc59d2370d390769a
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-20 16:52:27 +0900

    test2

---
+++ CMakeFiles/MyCExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCExecutable.dir/2048/2048.c.o"
+  "CMakeFiles/MyCExecutable.dir/2048/2048.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/BrickBreaker/Brickbreaker.c.o"
+  "CMakeFiles/MyCExecutable.dir/BrickBreaker/Brickbreaker.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/MineSweeper/Minesweeper.c.o"
+  "CMakeFiles/MyCExecutable.dir/MineSweeper/Minesweeper.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/Tetris/Tetris.c.o"
+  "CMakeFiles/MyCExecutable.dir/Tetris/Tetris.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/main.c.o"
+  "CMakeFiles/MyCExecutable.dir/main.c.o.d"
+  "MyCExecutable"
+  "MyCExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyCExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
