ef937f566b8cb8d2d144bf1727798aee9d099d47
great <greaths05@gmail.com>
2024-11-24 18:38:18 +0900

    벽돌깨기 <-> 메인화면 화면전환

--- OSS_TeamProject/games.h
+++ OSS_TeamProject/games.h
@@ -7,5 +7,8 @@ void start_breakout_game_BP();
 void start_minesweeper_game();
 
 void switch_to_main_menu(GtkWidget* widget, gpointer data);
+
 GtkWidget* create_minesweeper_screen(GtkStack* stack);
+GtkWidget* create_brickbreaker_screen(GtkStack* stack);
+
 #endif
