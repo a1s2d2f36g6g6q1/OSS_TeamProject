588ca554d7a476fffb667c57bc576d153ff5c7c0
great <greaths05@gmail.com>
2024-11-26 14:19:47 +0900

    index 수정 route 추가

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // CORS 모듈 가져오기
 const authRoutes = require('./route/auth'); // auth.js 가져오기
+const gameRoutes = require('./route/game');
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json()); // JSON 요청 처리
 app.use(cors()); // CORS 적용
+app.use(express.json({ limit: '1mb' }));
 
 app.use('/auth', authRoutes); // '/auth' 경로에 authRoutes 사용
+app.use('/game', gameRoutes);
+
+app.use((req, res, next) => {
+    res.setHeader('Connection', 'keep-alive');
+    next();
+});
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
