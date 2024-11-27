926cd6b42e4a8688a40b14cd47d04ad69efd8ab5
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-27 16:25:34 +0900

    restore

---
+++ cmake-build-debug/CMakeFiles/MyCExecutable.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/u4bi_/_Mac/Study/OSS_TeamProject/OSS_TeamProject/BP.c" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/BP.c.o" "gcc" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/BP.c.o.d"
+  "/Users/u4bi_/_Mac/Study/OSS_TeamProject/OSS_TeamProject/Tetris.c" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/Tetris.c.o" "gcc" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/Tetris.c.o.d"
+  "/Users/u4bi_/_Mac/Study/OSS_TeamProject/OSS_TeamProject/main.c" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/main.c.o" "gcc" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/main.c.o.d"
+  "/Users/u4bi_/_Mac/Study/OSS_TeamProject/OSS_TeamProject/mine.c" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/mine.c.o" "gcc" "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/mine.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
