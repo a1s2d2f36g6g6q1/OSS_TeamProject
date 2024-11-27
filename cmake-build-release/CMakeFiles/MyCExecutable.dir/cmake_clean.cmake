0667350014dbac3c86515807735bba13fdabe5bf
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-27 16:22:48 +0900

    asdf

---
+++ cmake-build-release/CMakeFiles/MyCExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/BP.c.o"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/BP.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/Tetris.c.o"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/Tetris.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/main.c.o"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/main.c.o.d"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/mine.c.o"
+  "CMakeFiles/MyCExecutable.dir/OSS_TeamProject/mine.c.o.d"
+  "MyCExecutable"
+  "MyCExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyCExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
