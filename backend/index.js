a4361dd6850b615c133eae9413153cdefbc82517
great <greaths05@gmail.com>
2024-12-02 16:44:11 +0900

    멀티 API 파일 생성 index에 수정 반영

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // CORS 모듈 가져오기
+const cors = require('cors');
+const http = require('http');
+const setupWebSocketServer = require('./websocket'); // WebSocket 서버 가져오기
+
 const authRoutes = require('./route/auth'); // auth.js 가져오기
+const gameRoutes = require('./route/game');
 
 const app = express();
-const PORT = 5000;
+const server = http.createServer(app); // HTTP 서버 생성
 
-app.use(bodyParser.json()); // JSON 요청 처리
-app.use(cors()); // CORS 적용
+const appPORT = 5000;
+const serverPORT = 6000;
 
-app.use('/auth', authRoutes); // '/auth' 경로에 authRoutes 사용
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+// Express 미들웨어 설정
+app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Connection', 'keep-alive');
+    next();
 });
-    
+app.use('/auth', authRoutes); // '/auth' 경로에 authRoutes 사용
+app.use('/game', gameRoutes);
+
+server.keepAliveTimeout = 60000; // 1 minute
+server.headersTimeout = 65000; // Slightly more than Keep-Alive timeout
+// WebSocket 서버 설정
+setupWebSocketServer(server);
+
+
+// HTTP 서버 실행
+server.listen(serverPORT,'0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${serverPORT}`);
+});
+app.listen(appPORT,'0.0.0.0', () => {
+    console.log(`app running on http://0.0.0.0:${appPORT}`);
+});
+
