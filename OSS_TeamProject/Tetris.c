3710054dbcf27090d8b75101e886597dee1c1960
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-12-09 13:55:05 +0900

    스코어 관련 에러 수정중

--- OSS_TeamProject/Tetris.c
+++ OSS_TeamProject/Tetris.c
@@ -8,6 +8,8 @@
 #define GRID_WIDTH 10
 #define GRID_HEIGHT 20
 
+
+
 int grid[GRID_HEIGHT][GRID_WIDTH] = {0}; // 게임판 데이터
 int current_tetromino[4][4];             // 현재 도형
 int current_x = GRID_WIDTH / 2 - 2;      // 도형의 X 위치
@@ -53,19 +55,19 @@ void update_tetris_score(int score, GtkWidget* score_label) {
 
 
 // 충돌 검사
-bool check_collision(int new_x, int new_y, int new_tetromino[4][4])
-{
-    for (int y = 0; y < 4; y++)
-    {
-        for (int x = 0; x < 4; x++)
-        {
-            if (new_tetromino[y][x] == 1)
-            {
+bool check_collision(int new_x, int new_y, int new_tetromino[4][4]) {
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            if (new_tetromino[y][x] == 1) {
                 int grid_x = new_x + x;
                 int grid_y = new_y + y;
 
-                if (grid_x < 0 || grid_x >= GRID_WIDTH || grid_y >= GRID_HEIGHT || grid[grid_y][grid_x] != 0)
-                {
+                // 배열 경계 초과 검사
+                if (grid_x < 0 || grid_x >= GRID_WIDTH || grid_y < 0 || grid_y >= GRID_HEIGHT) {
+                    return true; // 충돌 발생
+                }
+
+                if (grid[grid_y][grid_x] != 0) {
                     return true; // 충돌 발생
                 }
             }
@@ -104,24 +106,23 @@ void rotate_tetromino()
 // 새로운 도형 생성
 
 void spawn_new_tetromino() {
-    // 현재 블록을 다음 블록으로 대체
     memcpy(current_tetromino, next_tetromino, sizeof(current_tetromino));
 
-    // 다음 블록 생성
     int random_index = rand() % 7;
     memcpy(next_tetromino, tetrominos[random_index], sizeof(next_tetromino));
 
     current_x = GRID_WIDTH / 2 - 2;
     current_y = 0;
 
-    // 게임 오버 확인
     if (check_collision(current_x, current_y, current_tetromino)) {
         game_over_Tetris = true;
+        return;
     }
 
-    // 다음 블록 그리기 갱신
     if (GTK_IS_WIDGET(next_block_area)) {
-        gtk_widget_queue_draw(next_block_area);
+        gtk_widget_queue_draw(next_block_area); // 다음 블록 갱신
+    } else {
+        g_warning("spawn_new_tetromino: next_block_area가 유효하지 않습니다.");
     }
 }
 
@@ -191,11 +192,24 @@ gboolean draw_game_board(GtkWidget *widget, cairo_t *cr, gpointer data)
 
 // game_loop 수정
 gboolean game_loop(GtkWidget* widget, gpointer data) {
-    GtkWidget* score_label = GTK_WIDGET(data); // 전달된 score_label 가져오기
+    GtkWidget* score_label = GTK_WIDGET(data);
+
+    if (!GTK_IS_WIDGET(widget)) {
+        g_critical("game_loop: widget이 유효하지 않습니다!");
+        return FALSE;
+    }
+
+    if (!GTK_IS_LABEL(score_label)) {
+        g_critical("game_loop: score_label이 NULL이거나 유효하지 않습니다!");
+        return FALSE;
+    }
+
+    g_debug("game_loop: score_label은 유효합니다.");
 
     if (game_over_Tetris)
-        return FALSE; // 게임 루프 종료
+        return FALSE; // 게임 오버 시 루프 종료
 
+    // 도형 이동
     if (!check_collision(current_x, current_y + 1, current_tetromino)) {
         current_y++;
     } else {
@@ -209,18 +223,13 @@ gboolean game_loop(GtkWidget* widget, gpointer data) {
         }
 
         // 줄 삭제 및 점수 업데이트
-        clear_lines(score_label); // score_label 전달
+        clear_lines(score_label);
 
-        // 게임 오버 체크
-        for (int x = 0; x < GRID_WIDTH; x++) {
-            if (grid[0][x] != 0) {
-                game_over_Tetris = true;
-                break;
-            }
-        }
-
-        if (!game_over_Tetris) {
-            spawn_new_tetromino(); // 새 도형 생성
+        // 게임 오버 확인
+        if (check_collision(current_x, current_y, current_tetromino)) {
+            game_over_Tetris = true;
+        } else {
+            spawn_new_tetromino();
         }
     }
 
@@ -311,9 +320,10 @@ void clear_lines(GtkWidget* score_label) {
         }
     }
 
-    // 점수 계산 및 UI 업데이트
     if (cleared_lines > 0) {
         score_Tetris += cleared_lines * 100;  // 1줄 삭제당 100점
+        g_debug("clear_lines: %d 줄 삭제, 새로운 점수: %d", cleared_lines, score_Tetris);
+
         char score_text[16];
         snprintf(score_text, sizeof(score_text), "Score: %d", score_Tetris);
         gtk_label_set_text(GTK_LABEL(score_label), score_text);  // UI 점수 갱신
@@ -344,22 +354,37 @@ gboolean draw_next_tetromino(GtkWidget* widget, cairo_t* cr, gpointer data) {
 void on_start_button_clicked(GtkWidget* widget, gpointer data) {
     GtkWidget* drawing_area = GTK_WIDGET(data);
 
+    // 게임 루프 중단
     if (game_loop_id != 0) {
         g_source_remove(game_loop_id);
         game_loop_id = 0;
     }
 
-    memset(grid, 0, sizeof(grid)); // 그리드 초기화
+    // 게임 상태 초기화
+    memset(grid, 0, sizeof(grid));
     score_Tetris = 0;
     game_over_Tetris = false;
 
     GtkWidget* score_label = g_object_get_data(G_OBJECT(drawing_area), "score_label");
+    if (!GTK_IS_LABEL(score_label)) {
+        g_critical("on_start_button_clicked: score_label이 유효하지 않습니다!");
+        return;
+    }
+
     gtk_label_set_text(GTK_LABEL(score_label), "Score: 0");
+
+    if (!GTK_IS_WIDGET(next_block_area)) {
+        g_critical("on_start_button_clicked: next_block_area가 초기화되지 않았습니다!");
+        return;
+    }
+
+    int random_index = rand() % 7;
+    memcpy(next_tetromino, tetrominos[random_index], sizeof(next_tetromino));
 
     spawn_new_tetromino();
     gtk_widget_queue_draw(drawing_area);
 
-    game_loop_id = g_timeout_add(game_speed, (GSourceFunc)game_loop, score_label); // game_loop에 score_label 전달
+    game_loop_id = g_timeout_add(game_speed, (GSourceFunc)game_loop, score_label);
 }
 
 GtkWidget* create_tetris_screen(GtkStack* stack) {
@@ -378,6 +403,18 @@ GtkWidget* create_tetris_screen(GtkStack* stack) {
 
     // Drawing Area에 Score Label 저장
     g_object_set_data(G_OBJECT(drawing_area), "score_label", score_label);
+
+    // 다음 블록 표시 영역
+    next_block_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request(next_block_area, 100, 100); // 크기 설정
+    gtk_box_pack_start(GTK_BOX(vbox), next_block_area, FALSE, FALSE, 5);
+    g_signal_connect(next_block_area, "draw", G_CALLBACK(draw_next_tetromino), NULL); // 연결
+
+    if (!GTK_IS_WIDGET(next_block_area)) {
+        g_critical("create_tetris_screen: next_block_area 초기화 실패!");
+    } else {
+        g_debug("create_tetris_screen: next_block_area 초기화 완료!");
+    }
 
     // Start 버튼
     GtkWidget* start_button = gtk_button_new_with_label("Start");
